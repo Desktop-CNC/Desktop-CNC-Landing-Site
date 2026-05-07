0d3f04338f37db0ebceb99a130fcb4c26bc699bb
Matthew Papesh <55628349+Matthew-Papesh@users.noreply.github.com>
2026-05-07 15:16:53 -0400

    added code

--- desktop-cnc-site/RaspPiServer.ts
+++ desktop-cnc-site/RaspPiServer.ts
@@ -24,10 +24,9 @@ app.post('/launch-ugs', (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/:splat(*)', (req, res) => {
+app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
